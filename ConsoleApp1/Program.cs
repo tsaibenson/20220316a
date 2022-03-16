2b4b6c715a0459b9a2c03fe49fe5d2cef54439d5
Benson <c110196107@nkust.edu.tw>
2022-03-16 16:14:23 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.Write("請輸入你的分數:");
+            score = Convert.ToInt32(Console.ReadLine());
+            string result = score >= 60 ? "及格" : "不及格";
+            Console.WriteLine("何同學,這次你{0}", result);
+            Console.ReadLine();
+        }
+    }
+}
